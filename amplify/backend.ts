e3c90f4ba47e8b1ef521c98075282e1e44e05133
Yuda Ferry Mahendra <yuda.mahendra@shark.tech>
2024-12-30 16:55:40 +0700

    initial commit

---
+++ amplify/backend.ts
@@ -0,0 +1,20 @@
+import { defineBackend, secret } from '@aws-amplify/backend';
+// import * as cdk from "aws-cdk-lib";
+import * as nodejs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+console.log("========== process.argv ==========");
+console.log(process.argv);
+
+const backend = defineBackend({
+});
+
+const backendStack = backend.createStack('researchCDKonAmplify');
+
+new nodejs.NodejsFunction(backendStack, 'research_lambda', {
+  runtime: lambda.Runtime.NODEJS_20_X,
+  entry: 'amplify/research_lambda.ts',
+  handler: 'handler',
+  environment: {
+    NODE_ENV: secret('NODE_ENV').toString(),
+  }
+});
