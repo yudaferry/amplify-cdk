d7dcddf05e68b5fe67efd748a64d7f02b1e50b31
Yuda Ferry Mahendra <yuda.mahendra@shark.tech>
2024-12-31 18:02:34 +0700

    try to capture env variable

--- amplify/research_lambda.ts
+++ amplify/research_lambda.ts
@@ -1,3 +1,6 @@
-export const handler = () => {
-  return process.env.DB_CONNECTION;
+export const handler = (parameter: any) => {
+  const test = process.env.DB_CONNECTION;
+  console.log('DB_CONNECTION: ', test);
+  console.log('paramter: ', parameter);
+  return test;
 }
