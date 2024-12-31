830f837b44a7118ebd6ac6bfdc41acd466b8d424
Yuda Ferry Mahendra <yuda.mahendra@shark.tech>
2024-12-31 11:13:51 +0700

    using node env

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,11 @@
+import * as dotenv from "dotenv";
 import { defineBackend, secret } from '@aws-amplify/backend';
+
+const branch = process.env.GIT_BRANCH;
+const envPath = ['.env'];
+if (branch) envPath.push(`.env.${branch}`);
+dotenv.config({ path: envPath });
+
 // import * as cdk from "aws-cdk-lib";
 import * as nodejs from "aws-cdk-lib/aws-lambda-nodejs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
@@ -6,6 +13,7 @@ console.log("========== process.argv ==========");
 console.log(JSON.stringify(process.env, null, 2));
 console.log(process.env.GIT_BRANCH);
 console.log(process.env.AWS_BRANCH);
+console.log(process.env.DB_CONNECTION);
 const backend = defineBackend({
 });
 
@@ -16,6 +24,6 @@ new nodejs.NodejsFunction(backendStack, 'research_lambda', {
   entry: 'amplify/research_lambda.ts',
   handler: 'handler',
   environment: {
-    NODE_ENV: secret('NODE_ENV').toString(),
+    DB_CONNECTION: process.env.DB_CONNECTION as string | '',
   }
 });
